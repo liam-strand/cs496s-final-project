f821f8cb8c96605f10c9d32ab4f3f9610e51c800
Liam Strand <strandliam@gmail.com>
2025-11-11 14:01:18 -0600

    basic pyautogui test

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import pyautogui
+import time
+
+
 def main():
-    print("Hello from cs496s-final-project!")
+    print("Starting to press left arrow every second...")
+    print("Press Ctrl+C to stop")
+
+    try:
+        while True:
+            pyautogui.press("left")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nStopped pressing left arrow")
 
 
 if __name__ == "__main__":
